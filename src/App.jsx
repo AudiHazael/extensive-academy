114412123d5644d999cd6db708df019d7a898942
Hazael Audi <hazaelaudi@hmail.com>
2025-05-10 14:51:59 +0100

    caseChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/Home" element={<Home />} />
     </Routes>
   );
 }
